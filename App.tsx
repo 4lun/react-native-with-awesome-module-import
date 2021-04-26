d40bfee444bee243ab8aeee550c8e56b4180a0cc
Alun Davey <hey@4lun.io>
2021-04-26 21:17:10 +0100

    Base RN project with native module import

---
+++ App.tsx
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import AwesomeModule from 'react-native-awesome-module';
+
+const App = () => {
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const resp = await AwesomeModule.multiply(
+        Math.random() * 100,
+        Math.random() * 100,
+      );
+      setResult(resp);
+    })();
+  }, [setResult]);
+
+  return (
+    <SafeAreaView>
+      <Text style={styles.text}>Result: {result}</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    padding: 30,
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native with native module import
+
+Example of React Native project with import of custom native module (via npm).
+
+Native module lives in [separate repo](https://github.com/4lun/react-native-awesome-module) and is boostrapped using [Bob (npx create-react-native-library react-native-awesome-module)](https://github.com/callstack/react-native-builder-bob)
